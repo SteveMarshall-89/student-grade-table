3e9b4c779abd4b799ebca7710031885fe10a3170
Steve Marshall <stevenmarshall1989@gmail.com>
2020-02-06 11:41:17 -0800

    Completed the View All Grades feture set

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,6 +3,14 @@ class GradeTable {
     this.tableElement = tableElement;
   }
   updateGrades(grades) {
-    console.log(grades);
+    var tBody = $("tbody");
+    tBody.empty();
+    for (let i = 0; i < grades.length; i++) {
+      var tr = $("<tr>")
+        .append($("<td>").text(grades[i]["name"]))
+        .append($("<td>").text(grades[i]["course"]))
+        .append($("<td>").text(grades[i]["grade"]))
+      tBody.append(tr)
+    }
   }
 }
