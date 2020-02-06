24da9a6f552b3f9f6eda22a9373092dde0e2fad9
Steve Marshall <stevenmarshall1989@gmail.com>
2020-02-06 13:23:19 -0800

    Instantiated PageHeader class using header element as argument

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var table = document.querySelector("table");
+var header = document.querySelector("header");
+
+var pageHeaderInstance = new PageHeader(header);
 var gradeTableAgain = new GradeTable(table);
 
-var appObject = new App(gradeTableAgain);
+var appObject = new App(gradeTableAgain, pageHeaderInstance);
 appObject.start();
