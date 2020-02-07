f35bb48a4944ef6a7f92aa4672a397626b2c1742
Steve Marshall <stevenmarshall1989@gmail.com>
2020-02-07 13:05:19 -0800

    Modify App's start method to also pass this.deleteGrade into the onDeleteClick method of this.gradeTable

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var table = document.querySelector("table");
 var header = document.querySelector("header");
 var form = document.querySelector("form");
+var noGradesP = document.querySelector("p");
 
 var pageHeaderInstance = new PageHeader(header);
-var gradeTableAgain = new GradeTable(table);
+var gradeTableAgain = new GradeTable(table, noGradesP);
 var gradeFormInstance = new GradeForm(form);
 
 var appObject = new App(gradeTableAgain, pageHeaderInstance, gradeFormInstance);
