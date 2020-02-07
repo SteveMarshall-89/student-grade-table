6fb4dfecc13f6a441b0da909bd70cf4654ab3982
Steve Marshall <stevenmarshall1989@gmail.com>
2020-02-06 17:40:10 -0800

    Add the following to the beginning of main.js (before App is instantiated): Query the document for your form element. Instantiate your GradeForm, passing in the form element as an argument.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var table = document.querySelector("table");
 var header = document.querySelector("header");
+var form = document.querySelector("form");
 
 var pageHeaderInstance = new PageHeader(header);
 var gradeTableAgain = new GradeTable(table);
+var gradeFormInstance = new GradeForm(form);
 
 var appObject = new App(gradeTableAgain, pageHeaderInstance);
 appObject.start();
